3e9562c337c9d03cfa17eb147c2961057d342e92
andres.jaque@gmail.com <andres.jaque@gmail.com@c9dc133e-5dfb-0d31-bafb-a3c170f59236>
2012-03-19 15:24:06 +0000

    Asses Probability

--- trunk/matlab/expectedDistance.m
+++ trunk/matlab/expectedDistance.m
@@ -4,30 +4,28 @@ function [ E ] = expectedDistance ( instance, tau, l, qi)
 %The first tau must start and finish with 0, so: (0, ... ,0)
 %instance.d: [n+1,n+1] distance matrix
 
-%REVIEW:
-%What happen when a customer's demand is knowed, its probability is 1
-if l == instance.n %n is the last costumer in the tour, it is assumed sorted
+if l == instance.n %n is the last costumer in the tour
 	E = instance.d(tau(l+1)+1,1); %d(n,0)
 else
 	if l == 0
-		E = expectedDistance ( instance, tau, l+1, qi);%instead of l+1 put tau(l+1) 
+		E = expectedDistance ( instance, tau, l+1, qi);
     else
         %---
         auxE0 = instance.d(tau(l+1)+1, tau(l)+1);%d(i+1,i)
         for j=0:qi
             auxE0 = auxE0 + expectedDistance(instance,tau,l+1,qi-j)*...
-                (1/(instance.Cust(tau(l+1)).PD(2)-instance.Cust(tau(l+1)).PD(1)+1));%1/(b-a+1)
+                (1/probDemand (j, tau(l+1), instance));%1/(b-a+1)
         end
         for j=qi+1:instance.Cust(tau(l+1)).PD(2)
             auxE0 = auxE0 + (2*instance.d(1,tau(l+1)+1)+ ...%d(0,i+1)
                 expectedDistance(instance,tau,l+1,instance.Q+qi-j))*...
-                (1/(instance.Cust(tau(l+1)).PD(2)-instance.Cust(tau(l+1)).PD(1)+1));%1/(b-a+1)
+                (1/probDemand (j, tau(l+1), instance));%1/(b-a+1)
         end
         %---
         auxE1 = instance.d(1,tau(l)+1) + instance.d(1, tau(l+1)+1);%d(0,i)+d(0,i+1)
         for j=0:instance.Cust(tau(l+1)).PD(2)
             auxE1 = auxE1 + expectedDistance(instance,tau,l+1,instance.Q-j)*...
-                (1/(instance.Cust(tau(l+1)).PD(2)-instance.Cust(tau(l+1)).PD(1)+1));%1/(b-a+1)
+                (1/probDemand (j, tau(l+1), instance));%1/(b-a+1)
         end
         %---
         if(auxE0 >= auxE1)
@@ -38,4 +36,15 @@ else
     end	
 end
 
+end
+
+%Probability of customer i take the demand value k
+function [ p ] = probDemand (k, i, instance)
+    if( instance.Cust(i) == k )
+        p = 1;
+    elseif( instance.Cust(i) ~= -99 )
+        p = 0;
+    else
+        p = 1/(instance.Cust(i).PD(2)-instance.Cust(i).PD(1)+1);    
+    end
 end
