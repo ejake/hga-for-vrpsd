68d78bc413cb25f189a1c6e82359e457fb8723ba
andres.jaque@gmail.com <andres.jaque@gmail.com@c9dc133e-5dfb-0d31-bafb-a3c170f59236>
2012-03-20 00:51:44 +0000

    Updated

---
+++ trunk/matlab/dynamic_programming/State.m
@@ -0,0 +1,16 @@
+classdef State < handle
+    %STATE Summary of this class goes here
+    %   Detailed explanation goes here
+    
+    properties
+        l = 0;
+        q_l = 0;
+        r = [];
+    end
+    
+    methods
+        
+    end
+    
+end
+
