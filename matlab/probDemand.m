73db38530c4881a51e60c801c7ad2c27905c62ac
andres.jaque <andres.jaque@gmail.com>
2012-04-22 21:43:14 +0000

    Updating review Expected Distance

--- matlab/probDemand.m
+++ matlab/probDemand.m
@@ -1,11 +1,13 @@
 function [ p ] = probDemand (k, i, instance)
 %Probability of customer i take the demand value k
-    if( instance.Cust(i).D == k )
+    if( instance.Cust(i).D == k )% Demand is knowed and it's equal to k
         p = 1;
-    elseif( instance.Cust(i).D ~= -99 )
+    elseif( instance.Cust(i).D ~= -99 )% Demand is knowed and it's different of k
         p = 0;
-    else
+    elseif( k >= instance.Cust(i).PD(1) && k <= instance.Cust(i).PD(2) ) % Demand is unknowed and k exist in the probability distribution
         p = 1/(instance.Cust(i).PD(2)-instance.Cust(i).PD(1)+1);    
+    else
+        p = 0;
     end
 end
 
