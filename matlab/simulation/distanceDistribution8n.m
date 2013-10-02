5c18590a46106caa5b8acfd6aa0e27dc1df5f55e
andres.jaque <andres.jaque@gmail.com>
2013-10-02 13:33:31 +0000

    Expected distance algorithm with better control to q_l =0

--- matlab/simulation/distanceDistribution8n.m
+++ matlab/simulation/distanceDistribution8n.m
@@ -1,7 +1,7 @@
-function [frecDis, disDis] = distanceDistribution8n(policy, instance, cyclic)
+function [frecDis, disDis, distances] = distanceDistribution8n(policy, instance, cyclic)
 
-    cyclic = logical(cyclic);
-    numDemSum = 0;
+    %cyclic = logical(cyclic);
+    %numDemSum = 0;
     minDem = 0;
     maxDem = 0;
     for i = 1: instance.n        
@@ -11,8 +11,10 @@ function [frecDis, disDis] = distanceDistribution8n(policy, instance, cyclic)
     numDemSum = maxDem - minDem + 1;
     frecDis = zeros(1,numDemSum);
     disDis = zeros(1,numDemSum);
-    cases = 0;
+    %cases = 0;
     distot = 0;
+    iter = 1;
+    distances = zeros(1,(instance.Cust(1).PD(2)-instance.Cust(1).PD(1) + 1)*(instance.Cust(2).PD(2)-instance.Cust(2).PD(1) + 1)*(instance.Cust(3).PD(2)-instance.Cust(3).PD(1) + 1)*(instance.Cust(4).PD(2)-instance.Cust(4).PD(1) + 1)*(instance.Cust(5).PD(2)-instance.Cust(5).PD(1) + 1)*(instance.Cust(6).PD(2)-instance.Cust(6).PD(1) + 1)*(instance.Cust(7).PD(2)-instance.Cust(7).PD(1) + 1)*(instance.Cust(8).PD(2)-instance.Cust(8).PD(1) + 1));
     for x1 = instance.Cust(1).PD(1):instance.Cust(1).PD(2) % Demand of customer 1
         for x2 = instance.Cust(2).PD(1):instance.Cust(2).PD(2) % Demand of customer 2
             for x3 = instance.Cust(3).PD(1):instance.Cust(3).PD(2) % Demand of customer 3
@@ -23,40 +25,51 @@ function [frecDis, disDis] = distanceDistribution8n(policy, instance, cyclic)
                                 for x8 = instance.Cust(8).PD(1):instance.Cust(8).PD(2) % Demand of customer 8
                                     inst = [x1 x2 x3 x4 x5 x6 x7 x8];
                                     totDem = sum(inst);
-                                    cases = cases +1;
-                                    pos = 1;
+                                    pos = 0;
                                     pospol = 1;
                                     dist = 0;
                                     q = instance.Q;
                                     while (sum(inst) ~= 0)%asses distance
-                                        if ((q == 0) || (policy(pospol).a == 1))
+                                        if ((q == 0) || (policy(pospol).a == 1))%Proactive restocking when q is equal to 0
                                             dist = dist + instance.d(1,pos+1);
-                                            pos = 1;
+                                            pos = 0;
                                             q = instance.Q;
                                         end
-                                        nextpospol = pospol + 1;
-                                        if (nextpospol > instance.n) 
-                                            nextpospol = 1;
-                                        end
-                                        nextpos = policy(nextpospol).m;
-                                        dist = dist + instance.d(pos,nextpos);                            
+
+                                        %if pos == 0
+                                        %    nextpospol = 1;
+                                        %else
+                                        %    nextpospol = pospol + 1;
+                                        %end
+                                        %if (nextpospol > instance.n) 
+                                        %    nextpospol = 1;
+                                        %end
+
+                                        nextpos = policy(pospol).m;
+                                        dist = dist + instance.d(pos+1, nextpos+1);
                                         if ( q >= inst(nextpos)) 
                                             q = q -inst(nextpos);
                                             inst(nextpos) = 0; 
                                             pos = nextpos;
-                                            pospol = nextpospol;
+                                            pospol = nextpos + 1;
                                         else
                                             inst(nextpos) = inst(nextpos) - q;
                                             q = 0;
                                             pos = nextpos;
-                                            if(cyclic)
-                                                pospol = nextpospol; %uncomment if run cyclic tour
-                                            end
+                                            %if(cyclic)
+                                            %    pospol = nextpospol; %uncomment if run cyclic tour
+                                            %end
                                         end
+
+                                    end
+                                    if(pos ~= 0)%Add the distance to return to the depot 
+                                        dist = dist + instance.d(1,pos+1);
                                     end
                                     frecDis(totDem - minDem + 1) = frecDis(totDem - minDem + 1) + 1;
                                     disDis(totDem - minDem + 1) = dist;
                                     distot = distot + dist;
+                                    distances(iter) = dist;
+                                    iter = iter+1;
                                 end
                             end
                         end
