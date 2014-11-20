f3fb12be66617c236b5a8c3262fdd26a795ac2ae
andres.jaque <andres.jaque@gmail.com>
2014-11-20 18:18:27 +0000

    GA - bug correction in rollout algorithm

--- matlab/dynamic_programming/rollout.m
+++ matlab/dynamic_programming/rollout.m
@@ -27,7 +27,7 @@ function [pi cyEd]  = rollout( instance, state, tau )
             minl = tau(i+1);
             min_tau = tau;
         end
-        fprintf('tour evaluated [ %s ] (Expected distance since 0 : %6.4f)\n', num2str(tau), edl);
+        %fprintf('tour evaluated [ %s ] (Expected distance since 0 : %6.4f)\n', num2str(tau), edl);
         cyEd(l) = edl;
         tau = circshift(tau, [1,1]); % cyclic heuristic
     end
@@ -41,16 +41,20 @@ function [pi cyEd]  = rollout( instance, state, tau )
     
     %computing the remaining customers to be visited by pi
     while ( ~ x.isFinalState )
-        for j = 1:length(sNu) % for each node unvisited ( sNu(j) ) asses expected distance for partial tour
+        minEd = inf;
+        for j = 1:length(sNu) % for each node unvisited ( sNu(j) ) asses expected distance for partial tour            
+            %fprintf('tour evaluated [ %s ], l %i, sNu [ %s ]',num2str(tau),i,num2str(sNu));
             J0 = cost2goBackwardJ(instance, tau, i, x.q_l, 0);
             J1 = cost2goBackwardJ(instance, tau, i, x.q_l, 1);
             [edl a] = min([J0 J1]);
+            %fprintf(',J0 %6.4f, J1 %6.4f, edl %6.4f', J0,J1,edl)
             if(edl < minEd)
                 minEd = edl;
                 minl = tau(i+1);
                 mina = a - 1;
                 min_tau = tau;
             end
+            %fprintf(', minl %i\n',minl);
             %fprintf('tour evaluated [ %s ](Expected distance since %i : %6.4f )\n', num2str(tau), tau(i), edl);
             tau = [tau(1:i) circshift(tau(i+1:instance.n), [1,1])]; % cyclic heuristic applied to tau subsequence
         end
